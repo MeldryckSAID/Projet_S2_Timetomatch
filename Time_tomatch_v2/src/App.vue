da68f160c12629f928c676248899bca021b97401
MeldryckSAID <meldryck.said@edu.univ-fcomte.fr>
2022-05-11 16:56:28 +0200

    premiere config

--- Time_tomatch_v2/src/App.vue
+++ Time_tomatch_v2/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
@@ -11,17 +10,21 @@ import HelloWorld from '@/components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Accueil</RouterLink>
+        
       </nav>
+        <RouterView />
     </div>
   </header>
 
-  <RouterView />
+  
 </template>
+
+
 
 <style>
 @import '@/assets/base.css';
+
 
 #app {
   max-width: 1280px;
--- Time_tomatch_v2/src/views/AccueilView.vue
+++ Time_tomatch_v2/src/views/AccueilView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <h1 class="bg-gray-200"> test  </h1>
   </main>
 </template>
